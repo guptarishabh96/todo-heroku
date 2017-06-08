1ab7b050d49a29f2b37a9bd927e45419f5c79527
guptarishabh96 <guptarishabh96@gmail.com>
2017-06-08 15:47:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+var mongoose=require('mongoose');
+
+var todo = [ {term: "Sample Item 1"}, {term: "Sample Item 2"}, {term: "Sample Item 3"} ];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+	next();
+});
+
+app.use(express.static("./public"));
+
+app.use(cors());
+
+app.get("/todo", function(req, res) {
+	res.json(todo);
+});
+
+app.post("/todo", function(req, res) {
+    todo.push(req.body);
+    res.json(todo);
+});
+
+app.delete("/todo/:term", function(req, res) {
+    todo = todo.filter(function(definition) {
+        return definition.term.toLowerCase() !== req.params.term.toLowerCase();
+    });
+    res.json(todo);
+});
+
+app.listen(process.env.PORT);
+
+console.log("Express app running on heroku");
+
+var db = mongoose.createConnection('mongodb://localhost/todolist',function(err){
+    if(err){
+        console.log(err);
+    } else{
+        console.log('Connected to todo list db!');
+    }
+});
+
+var todoSchema = mongoose.Schema({
+    item: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Item = db.model('Item', todoSchema);
+
+module.exports = app;
